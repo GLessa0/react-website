c0dd72f6e1dfacfdc6e87f61a6ea8083898552ba
Gabriel_lessa <gabrielmirandalessa@gmail.com>
2025-11-04 11:25:45 -0300

    Add NavBar component

---
+++ react-website/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function NavBar() {
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+            <link to='/' className='navbar-logo'>
+                MYAPP
+            </link>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
